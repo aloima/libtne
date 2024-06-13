283a3c255faf7293cff6dbee133cb71c8211e8f2
aloima <acar.aloima@gmail.com>
2024-06-13 21:23:12 +0300

    add simple http request without tls support for http1.1
    add catching status message and status code

---
+++ lib/http.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#include "../tne.h"
+
+tneresponse_t *tne_request(tnerequest_t request) {
+  tneresponse_t *response = malloc(sizeof(tneresponse_t));
+  memset(response, 0, sizeof(tneresponse_t));
+
+  struct hostent *host = gethostbyname(request.url.hostname);
+  int fd;
+
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    fputs("tne_request: socket opening error\n", stderr);
+    return NULL;
+  }
+
+  struct sockaddr_in addr = {
+    .sin_family = AF_INET,
+    .sin_port = htons(request.url.port),
+    .sin_addr = {
+      .s_addr = *(long *) (host->h_addr)
+    }
+  };
+
+  if (connect(fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+    fputs("tne_request: connecting error\n", stderr);
+    return NULL;
+  }
+
+  char request_message[12288];
+  size_t request_message_length = sprintf(request_message, (
+    "%s %s HTTP/1.1\r\n"
+    "Host: %s\r\n"
+    "Connection: close\r\n\r\n"
+  ), request.method, request.url.path, request.url.hostname);
+
+  if (send(fd, request_message, request_message_length, 0) == -1) {
+    fputs("tne_request: sending error\n", stderr);
+    return NULL;
+  }
+
+  unsigned char received[2048];
+  unsigned int received_size = recv(fd, received, sizeof(received_size), 0);
+  unsigned long long response_message_size = received_size;
+  char *response_message = malloc(response_message_size);
+
+  memcpy(response_message, received, received_size);
+
+  while ((received_size = recv(fd, received, sizeof(received), 0)) > 0) {
+    int new_size = response_message_size + received_size;
+    response_message = realloc(response_message, new_size);
+    memcpy(response_message + response_message_size, received, received_size);
+    response_message_size = new_size;
+  }
+
+  char status_string[4];
+  tne_strncpy(status_string, response_message + 9, 3);
+  response->status.code = atoi(status_string);
+
+  for (unsigned int i = 13; i < response_message_size; ++i) {
+    if (response_message[i] == '\r') {
+      response->status.message_length = i - 13;
+      response->status.message = malloc(response->status.message_length + 1);
+      tne_strncpy(response->status.message, response_message + 13, response->status.message_length);
+      break;
+    }
+  }
+
+  free(response_message);
+  close(fd);
+
+  return response;
+}
+
+tnerequest_t tne_prepare_request(char *method, char *url) {
+  unsigned int method_size = strlen(method) + 1;
+  tnerequest_t request = {
+    .method = malloc(method_size),
+    .url = tne_parse_url(url)
+  };
+
+  memcpy(request.method, method, method_size);
+  return request;
+}
+
+void tne_free_request(tnerequest_t request) {
+  tne_free_url(request.url);
+  free(request.method);
+}
+
+void tne_free_response(tneresponse_t *response) {
+  tne_free_headers(response->headers);
+  free(response->status.message);
+  free(response);
+}
--- tne.h
+++ tne.h
@@ -7,7 +7,7 @@
     char *hostname;
     char *path;
     char *protocol;
-    unsigned int port;
+    unsigned short port;
   } tneurl_t;
 
   typedef struct {
@@ -15,10 +15,31 @@
     char *value;
   } tneheader_t;
 
+  typedef struct {
+    int code;
+    char *message;
+    unsigned int message_length;
+  } tnestatus_t;
+
   struct TNEHeaders {
     tneheader_t *headers;
     size_t header_count;
   };
+
+  typedef struct {
+    char *method;
+    tneurl_t url;
+    struct TNEHeaders headers;
+    char *data;
+    unsigned long long data_size;
+  } tnerequest_t;
+
+  typedef struct {
+    struct TNEHeaders headers;
+    char *data;
+    unsigned long long data_size;
+    tnestatus_t status;
+  } tneresponse_t;
 
   tneurl_t tne_parse_url(char *url);
   void tne_free_url(tneurl_t url);
@@ -27,6 +48,11 @@
   void tne_remove_header(struct TNEHeaders *dest, char *name);
   tneheader_t *tne_get_header(struct TNEHeaders dest, char *name);
   void tne_free_headers(struct TNEHeaders dest);
+
+  tneresponse_t *tne_request(tnerequest_t request);
+  tnerequest_t tne_prepare_request(char *method, char *url);
+  void tne_free_request(tnerequest_t request);
+  void tne_free_response(tneresponse_t *response);
 
   void tne_strncpy(char *dest, char *src, size_t n);
 #endif
